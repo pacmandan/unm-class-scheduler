c61d3902f825eb9101029d05076161a1d7c9de30
Daniel Green <dgreen314@gmail.com>
2023-07-27 19:46:12 -0600

    Moved the fixtures into a test seeds file, finally got some tests for the BuildQuery module.

--- test/support/fixtures.ex
+++ test/support/fixtures.ex
@@ -8,15 +8,19 @@ defmodule UnmClassScheduler.Fixtures do
 
   alias UnmClassScheduler.Repo
 
+  @spec build(list(Ecto.Schema.t()), (Ecto.Schema.t() -> any())) :: %{any() => Ecto.Schema.t()}
   def build(params, key_fn \\ &code_key/1) do
     Enum.map(params, &Repo.insert/1)
+    |> Enum.map(fn {:ok, d} -> d end)
     |> Enum.map(fn d -> {key_fn.(d), d} end)
     |> Enum.into(%{})
   end
 
+  @spec build_no_key(Ecto.Schema.t()) :: list(Ecto.Schema.t())
   def build_no_key(params) do
     Enum.map(params, &Repo.insert/1)
   end
 
+  @spec code_key(Ecto.Schema.t()) :: String.t()
   def code_key(data), do: data.code
 end
