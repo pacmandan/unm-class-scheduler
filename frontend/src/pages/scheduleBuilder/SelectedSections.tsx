a564379df45a71fcb812fe6ddcf6e9e4ad0db449
Daniel Green <dgreen314@gmail.com>
2023-09-02 21:13:24 -0600

    Added a number of selected sections counter.

--- frontend/src/pages/scheduleBuilder/SelectedSections.tsx
+++ frontend/src/pages/scheduleBuilder/SelectedSections.tsx
@@ -3,19 +3,21 @@ import { SelectedSection } from '@/catalog';
 import { useSelector, useDispatch } from 'react-redux';
 import { removeSection } from '@/features/schedule';
 import ResultRow from './ResultRow';
+
 function SelectedSections () {
   const scheduleState = useSelector((store: RootState) => store.schedule)
 
   return (<div className='overflow-y-auto h-full'>
-    {Object.entries(scheduleState.selected).map(([_crn, section]) => {return(<SelectedRow section={section}/>)})}
+    <p className='m-1'>{Object.entries(scheduleState.selected).length} Sections Selected</p>
+    {Object.entries(scheduleState.selected).map(([_crn, selected]) => {return(<SelectedRow selected={selected}/>)})}
   </div>)
 }
 
-const SelectedRow = ({section}: {section: SelectedSection}) => {
+const SelectedRow = ({selected}: {selected: SelectedSection}) => {
   const dispatch = useDispatch<AppDispatch>()
 
   const dropResult = () => {
-    dispatch(removeSection(section.section))
+    dispatch(removeSection(selected.section))
   }
 
   return(<div style={{gridTemplateAreas:'\
@@ -25,8 +27,8 @@ const SelectedRow = ({section}: {section: SelectedSection}) => {
     ". display" \
   '}} className='grid grid-rows-[1fr_60px_60px_1fr] grid-cols-[30px_1fr]'>
     <div style={{'gridArea':'close'}}><button onClick={dropResult} className='border-2 w-6 h-10 m-1 bg-slate-100 font-bold'>X</button></div>
-    <div style={{'gridArea':'color'}}><button style={{backgroundColor: section.color}} className={`border-2 w-6 h-10 m-1`}></button></div>
-    <div style={{'gridArea':'display'}}><ResultRow section={section.section}/></div>
+    <div style={{'gridArea':'color'}}><button style={{backgroundColor: selected.color}} className={`border-2 w-6 h-10 m-1`}></button></div>
+    <div style={{'gridArea':'display'}}><ResultRow section={selected.section}/></div>
   </div>)
 }
 
