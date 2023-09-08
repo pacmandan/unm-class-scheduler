77f3e6148f245a238edfe00e39aa6ce6d6a68187
Daniel Green <dgreen314@gmail.com>
2023-09-08 12:42:48 -0600

    Turns out the way I did the Home page doesn't work
    The endpoint needs to be different in dev vs prod.
    Apparently the way to do this is to put images into "assets"
    and load them via "import" into the components.
    
    Hopefully this works. Another unrelated bugfix on this branch.

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,13 +1,15 @@
+import placeholderUrl from '@/assets/placeholder.png'
+
 function HomePage() {
   return (<>
     <div className="bg-red-800 w-full h-96 relative mb-4">
-      <img src="/placeholder.png" alt="Cover" width="100%" height="24rem" className="w-full h-96 object-cover" />
+      <img src={placeholderUrl} alt="Cover" width="100%" height="24rem" className="w-full h-96 object-cover" />
       <h1 className="font-black font-sans text-5xl text-white absolute text-center left-28 top-36">Plan your class schedule</h1>
       <button><a href="/scheduleBuilder" className="absolute left-72 top-52 text-xl font-bold font-sans bg-red-800 p-3">Plan Schedule</a></button>
     </div>
     <div className='w-full h-96 relative mb-4 p-10 flex items-center justify-center'>
       <div className='w-[750px] flex items-center justify-center'>
-        <img src="/placeholder.png" alt="feature1" className="w-72 h-72 object-cover" />
+        <img src={placeholderUrl} alt="feature1" className="w-72 h-72 object-cover" />
         <div className='w-10'></div>
         <div className='m-5 w-full'>
           <h2 className='text-3xl font-bold'>See your schedule on a calendar</h2>
@@ -22,12 +24,12 @@ function HomePage() {
           <p>Plug in what classes you want, and get back every possible way those classes can fit together without overlap</p>
         </div>
         <div className='w-10'></div>
-        <img src="/placeholder.png" alt="feature2" className="w-72 h-72 object-cover" />
+        <img src={placeholderUrl} alt="feature2" className="w-72 h-72 object-cover" />
       </div>
     </div>
     <div className='w-full h-96 relative mb-4 p-10 flex items-center justify-center'>
       <div className='w-[750px] flex items-center justify-center'>
-        <img src="/placeholder.png" alt="feature3" className="w-72 h-72 object-cover" />
+        <img src={placeholderUrl} alt="feature3" className="w-72 h-72 object-cover" />
         <div className='w-10'></div>
         <div className='m-5'>
           <h2 className='text-3xl font-bold'>Future Feature: Directions</h2>
